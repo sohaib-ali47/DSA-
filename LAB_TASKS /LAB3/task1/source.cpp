9c0eb1ed0ed127c4608d845c51e2e7346f706386
Sohaib_ali <165940210+sohaib-ali47@users.noreply.github.com>
2025-04-06 18:50:14 +0500

    Update source.cpp
    source file of my code

--- LAB_TASKS /LAB3/task1/source.cpp
+++ LAB_TASKS /LAB3/task1/source.cpp
@@ -1,1 +1,22 @@
-hi
+
+#include "derivedclass.h"
+
+int main() {
+    List<int>* ptr = new Work<int>(10, 0);  
+
+    cout << "ading element at first  "<<endl;
+    ptr->addElementAtFirstIndex(5);
+    ptr->display();
+    cout << "ading at last " << endl;
+    ptr->addElementAtLastIndex(10);
+    ptr->display();
+    cout << "removing from end  " << endl;
+    ptr->removeElementFromEnd(); 
+    ptr->display();
+    cout << "removing from start  " << endl;
+    ptr->  removeElementFromStart();
+    ptr->display();
+    delete ptr;  // Clean up memory
+
+    return 0;
+}
