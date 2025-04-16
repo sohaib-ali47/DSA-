6be185fa197306db7fe304a3806109fc7a44bc9f
Sohaib_ali <165940210+sohaib-ali47@users.noreply.github.com>
2025-04-16 21:42:12 +0500

    Add files via upload

---
+++ LAB_TASKS /lab4/task3/car_stack.h
@@ -0,0 +1,78 @@
+#pragma once
+#include <iostream>
+#include <string>
+using namespace std;
+
+class myCarStack {
+private:
+    string cars[8];
+    int topIndex;
+
+public:
+    myCarStack() {
+        topIndex = -1;
+    }
+
+    bool isFull() {
+        return topIndex == 7;
+    }
+
+    bool isEmpty() {
+        return topIndex == -1;
+    }
+
+    void push(string car) {
+        if (!isFull()) {
+            topIndex++;
+            cars[topIndex] = car;
+        }
+        else {
+            cout << "Parking is full!" << endl;
+        }
+    }
+
+    string pop() {
+        if (!isEmpty()) {
+            string car = cars[topIndex];
+            topIndex--;
+            return car;
+        }
+        else {
+            return "";
+        }
+    }
+
+    string top() {
+        if (!isEmpty()) {
+            return cars[topIndex];
+        }
+        else {
+            return "No car";
+        }
+    }
+
+    int count() {
+        return topIndex + 1;
+    }
+
+    bool findCar(string carNumber) {
+        for (int i = 0; i <= topIndex; i++) {
+            if (cars[i] == carNumber) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    string getCarAt(int i) {
+        return cars[i];
+    }
+
+    void setCarAt(int i, string car) {
+        cars[i] = car;
+    }
+
+    int getTopIndex() {
+        return topIndex;
+    }
+};
