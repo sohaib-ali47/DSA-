962b4f1249feb0983d87b40046527676d5ea878d
Sohaib_ali <165940210+sohaib-ali47@users.noreply.github.com>
2025-05-01 00:04:16 +0500

    Update main.cpp

--- LAB_TASKS /lab7/task2/main.cpp
+++ LAB_TASKS /lab7/task2/main.cpp
@@ -1,1 +1,23 @@
 
+# include "linked_list.h"
+
+int main() {
+
+	Linked_list l1;
+	l1.push_front(5);
+	l1.push_front(8);
+	l1.push_front(10);
+	l1.push_back(100);
+	l1.delete_by_value(10);
+	l1.display();
+
+
+
+
+
+
+
+
+
+	return 0;
+}
