5b1c1afb596b305a97f148ca1354609534b7a48f
Sohaib_ali <165940210+sohaib-ali47@users.noreply.github.com>
2025-05-20 09:29:23 +0500

    Update main.cpp

--- recursion /example/factorial /main.cpp
+++ recursion /example/factorial /main.cpp
@@ -1,1 +1,22 @@
+#include <iostream>
+using namespace std;
 
+// Recursive function to calculate factorial
+int factorial(int n) {
+    if (n == 0 || n == 1) {       // Base case
+        return 1;
+    } else {
+        return n * factorial(n - 1); // Recursive call
+    }
+}
+
+int main() {
+    int num;
+    cout << "Enter a number: ";
+    cin >> num;
+
+    int result = factorial(num);
+    cout << "Factorial of " << num << " is: " << result << endl;
+
+    return 0;
+}
