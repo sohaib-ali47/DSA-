26d1f31cd63ff74b17425e28f93c8f4d016bdfaf
Sohaib_ali <165940210+sohaib-ali47@users.noreply.github.com>
2025-05-24 23:09:48 +0500

    Update main.cpp

--- LAB_TASKS /lab8 /main.cpp
+++ LAB_TASKS /lab8 /main.cpp
@@ -1,18 +1,31 @@
+#include <iostream>
+#include "Linked_list.h"
+using namespace std;
 
+int main() {
+    CircularLinkedList<int> obj1;
+    int choice = 0;
 
-# include "linked_list.h"
+    while (choice != 99) {
+        cout << "Press 1 to start the game" << endl;
+        cout << "Press 99 to exit" << endl;
+        cout << "Enter your choice: ";
+        cin >> choice;
 
+        if (choice == 1) {
+            int start, skip;
+            cout << "Enter starting person number: ";
+            cin >> start;
+            cout << "Enter number of persons to skip: ";
+            cin >> skip;
 
-int main()
-{
-	Circular_queue q1; 
+            obj1.clearList();
+            obj1.createList();
 
+            int survivor = obj1.solveJosephus(start, skip + 1);
+            cout << "The winner  is: " << survivor << endl;
+        }
+    }
 
-
-
-
-
-
-
-	return 0; 
+    return 0;
 }
