21f5b7afb152eb08aaee8baaebfd6b551a0449bf
Sohaib_ali <165940210+sohaib-ali47@users.noreply.github.com>
2025-06-07 21:42:15 +0500

    Create main.cpp

---
+++ LAB_TASKS /lab_10_recursion/task4/main.cpp
@@ -0,0 +1,21 @@
+
+
+
+
+# include "lab_10_l.s.h"
+
+
+
+
+int main () {
+
+	linked_list <int> l1;
+
+	l1.add_at_tail(1);
+	l1.add_at_tail(2);
+	l1.add_at_tail(3);
+	l1.display();
+
+
+	return 0; 
+}
