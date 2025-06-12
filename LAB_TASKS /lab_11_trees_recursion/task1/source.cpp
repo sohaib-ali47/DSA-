6786f5698bcb984ab84d3c28dab4f5568166fb6f
Sohaib_ali <165940210+sohaib-ali47@users.noreply.github.com>
2025-06-12 22:19:44 +0500

    Update source.cpp

--- LAB_TASKS /lab_11_trees_recursion/task1/source.cpp
+++ LAB_TASKS /lab_11_trees_recursion/task1/source.cpp
@@ -1,1 +1,28 @@
-temp file 
+# include "task1_trees.h"
+
+
+
+int main() {
+
+
+	tree t1;
+
+	t1.insert(5);
+	t1.insert(2);
+	t1.insert(6);
+	t1.insert(7);
+	t1.insert(1);
+
+	t1.pre_trav();
+	t1.in_trav();
+	t1.post_trav();
+
+
+
+
+
+
+
+
+	return 0; 
+}
