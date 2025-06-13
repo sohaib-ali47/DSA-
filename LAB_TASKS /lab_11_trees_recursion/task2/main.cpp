bc6548ae4fdab393d477a8d43e91595334fdecd0
Sohaib_ali <165940210+sohaib-ali47@users.noreply.github.com>
2025-06-13 22:53:17 +0500

    Update main.cpp

--- LAB_TASKS /lab_11_trees_recursion/task2/main.cpp
+++ LAB_TASKS /lab_11_trees_recursion/task2/main.cpp
@@ -1,1 +1,18 @@
+#include "task2_tree.h"
 
+int main() {
+    tree t;
+
+    t.insert(50);
+    t.insert(30);
+    t.insert(70);
+    t.insert(20);
+    t.insert(40);
+    t.insert(60);
+    t.insert(80);
+
+    t.
+    t.in_trav();  // Should print: 20 30 40 50 60 70 80
+
+    return 0;
+}
