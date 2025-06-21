3929d9ddae14dc2e76aecabbed239e942a9f0b6a
Sohaib_ali <165940210+sohaib-ali47@users.noreply.github.com>
2025-06-21 23:04:32 +0500

    Update source.cpp
    main file

--- LAB_TASKS /lab_11_trees_recursion/task4/source.cpp
+++ LAB_TASKS /lab_11_trees_recursion/task4/source.cpp
@@ -1,1 +1,20 @@
+#include "task4_tree.h"
 
+int main() {
+    tree t;
+
+    t.insert(40);
+    t.insert(20);
+    t.insert(60);
+    t.insert(10);
+    t.insert(30);
+    t.insert(50);
+    t.insert(70);
+
+   
+
+    cout << "Minimum value in BST: " << t.findMin() << endl;
+    cout << "Maximum value in BST: " << t.findMax() << endl;
+
+    return 0;
+}
