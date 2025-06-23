267fca46e6f933a4c6e839d4267cb17dfa8da270
Sohaib_ali <165940210+sohaib-ali47@users.noreply.github.com>
2025-06-23 22:40:51 +0500

    Update source.cpp

--- LAB_TASKS /lab_11_trees_recursion/task5/source.cpp
+++ LAB_TASKS /lab_11_trees_recursion/task5/source.cpp
@@ -1,1 +1,29 @@
-.
+#include "task5_tree.h"
+
+int main() {
+    tree t;
+
+    // Sample insertion
+    t.insert(50);
+    t.insert(30);
+    t.insert(70);
+    t.insert(20);
+    t.insert(40);
+    t.insert(60);
+    t.insert(80);
+
+    cout << "Original tree: ";
+    t.in_trav();
+
+    // Ask user for value to delete
+    int val;
+    cout << "Enter value to delete: ";
+    cin >> val;
+
+    t.remove(val);
+
+    cout << "Updated tree after deletion: ";
+    t.in_trav();
+
+    return 0;
+}
